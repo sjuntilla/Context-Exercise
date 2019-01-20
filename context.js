e36cbb64a152c2e5d0f01e38225c6a4363bb8560
Samantha Juntilla <juntilla@hawaii.edu>
2019-01-19 17:24:16 -1000

    finished.

--- context.js
+++ context.js
@@ -3,10 +3,11 @@ var Robot = function (name) {
 
 }
 
-function add ( op1, op2 ) {
+function add(op1, op2) {
   this.name = this.name || "Humans";
   return this.name + " can count to " + (op1 + op2);
 }
+
 
 var voltron = new Robot("Voltron");
 var bender = new Robot("Bender Rodriguez");
@@ -17,14 +18,14 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log(add(0, 1));
 
 // #2  
-console.log(  );
+console.log(add.call(voltron, 2, 3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus, [20, 30]));
 
 // #4
-
-console.log(  );
+const calculate = add.bind(bender);
+console.log(calculate('drinking ', 'beer'));
